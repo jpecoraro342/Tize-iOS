4ea4e6a421a1930341c58c3f99fbdb06643914ee
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-06-17 03:16:31 -0400

    Initial Commit

---
+++ Tize/Tize/GWTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GWTAppDelegate.h
+//  Tize
+//
+//  Created by Joseph Pecoraro on 6/17/14.
+//  Copyright (c) 2014 GrayWolfTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GWTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tize/Tize/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tize
+//
+//  Created by Joseph Pecoraro on 6/17/14.
+//  Copyright (c) 2014 GrayWolfTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GWTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GWTAppDelegate class]));
+    }
+}
