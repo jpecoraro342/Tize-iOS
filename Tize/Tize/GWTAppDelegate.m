e777a12e79b9ca173c7e320d6e56538641aa41a9
Joseph Pecoraro <jpecoraro342@ufl.edu>
2014-06-17 03:26:58 -0400

    Added parse dependencies

--- Tize/Tize/GWTAppDelegate.m
+++ Tize/Tize/GWTAppDelegate.m
@@ -7,13 +7,14 @@
 //
 
 #import "GWTAppDelegate.h"
+#import <Parse/Parse.h>
 
 @implementation GWTAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
