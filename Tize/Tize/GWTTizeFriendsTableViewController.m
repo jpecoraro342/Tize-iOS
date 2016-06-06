279abefac546c8f436b856233dc11dbc7b981b8d
jpecoraro342 <jpecoraro342@ufl.edu>
2016-06-06 15:38:47 -0400

    Fixed my tize page. Added ability to easily follow users that are following you.

--- Tize/Tize/GWTTizeFriendsTableViewController.m
+++ Tize/Tize/GWTTizeFriendsTableViewController.m
@@ -15,11 +15,9 @@
 
 @property (nonatomic, strong) NSMutableArray* listOfFriendsWhoAddedMe;
 @property (nonatomic, strong) NSMutableArray* listOfFriendsIveAdded;
-@property (nonatomic, strong) NSMutableArray* peopleImFollowingNotFollowingMe;
 
 @property (nonatomic, strong) NSMutableArray* filteredListOfFriendsWhoAddedMe;
 @property (nonatomic, strong) NSMutableArray* filteredListOfFriendsIveAdded;
-@property (nonatomic, strong) NSMutableArray* filteredPeopleImFollowingNotFollowingMe;
 
 @property (nonatomic, strong) NSMutableDictionary* friendsWhoAddedMe;
 @property (nonatomic, strong) NSMutableDictionary* friendsIveAdded;
@@ -68,9 +66,9 @@
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     switch (section) {
         case 0:
-            return [super searchIsActive] ? [self.filteredListOfFriendsWhoAddedMe count] : [self.listOfFriendsWhoAddedMe count];
+            return [self.getListOfFriendsIveAdded count];
         case 1:
-            return [super searchIsActive] ? [self.filteredPeopleImFollowingNotFollowingMe count] : [self.peopleImFollowingNotFollowingMe count];
+            return [self.getListOfFriendsWhoAddedMe count];
     }
     return 0;
 }
@@ -78,10 +76,10 @@
 -(NSString*)titleForHeaderInSection:(NSInteger)section {
     switch (section) {
         case 0: {
-            return @"Tize Requests";
+            return @"Following";
         }
         case 1: {
-            return @"Friends Not Responded";
+            return @"Followers";
         }
     }
     
@@ -101,18 +99,18 @@
     
     switch (indexPath.section) {
         case 0: {
-            if (indexPath.row < [self.listOfFriendsWhoAddedMe count]) {
-                PFUser *userFollowing = [super searchIsActive] ? [self.filteredListOfFriendsWhoAddedMe objectAtIndex:indexPath.row] : [self.listOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
+            if (indexPath.row < [self.listOfFriendsIveAdded count]) {
+                PFUser *userFollowing = [self.getListOfFriendsIveAdded objectAtIndex:indexPath.row];
                 cell.textLabel.text = [userFollowing username];
-                cell.accessoryType = [self.friendsIveAdded objectForKey:[userFollowing objectId]] ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+                cell.accessoryType = [self.friendsWhoAddedMe objectForKey:[userFollowing objectId]] ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
             }
             break;
         }
         case 1: {
-            if (indexPath.row < [self.peopleImFollowingNotFollowingMe count]) {
-                PFUser *userImFollowing = [super searchIsActive] ? [self.peopleImFollowingNotFollowingMe objectAtIndex:indexPath.row] : [self.peopleImFollowingNotFollowingMe objectAtIndex:indexPath.row];
+            if (indexPath.row < [self.listOfFriendsWhoAddedMe count]) {
+                PFUser *userImFollowing = [self.getListOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
                 cell.textLabel.text = [userImFollowing username];
-                cell.accessoryType = UITableViewCellAccessoryNone;
+                cell.accessoryType = [self.friendsIveAdded objectForKey:[userImFollowing objectId]] ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
             }
             break;
         }
@@ -124,15 +122,39 @@
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
-    if (indexPath.section == 0) {
-        PFUser *friend = [super searchIsActive] ? [self.filteredListOfFriendsWhoAddedMe objectAtIndex:indexPath.row] : [self.listOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
-        if ([self.friendsIveAdded objectForKey:[friend objectId]]) {
-            [self removeFriendAtIndexPath:indexPath];
-        }
-        else {
-            [self addFriendAtIndexPath:indexPath];
-        }
+    // TODO: View the users "profile", their upcoming events, or something of the sort
+}
+
+-(NSArray<UITableViewRowAction*>*)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath {
+    PFUser *rowUser = indexPath.section == 0 ? [self.getListOfFriendsIveAdded objectAtIndex:indexPath.row] : [self.getListOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
+    
+    if ([self.friendsIveAdded objectForKey:[rowUser objectId]]) {
+        return @[[self stopFollowingUserRowAction: rowUser]];
     }
+    
+    return @[[self followUserRowAction: rowUser]];
+}
+
+-(UITableViewRowAction*)followUserRowAction:(PFUser *)user {
+    UITableViewRowAction *rowAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"Follow" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        [self confirmFollowUser:user];
+    }];
+    
+    rowAction.backgroundColor = kGreenColor;
+    return rowAction;
+}
+
+-(UITableViewRowAction*)stopFollowingUserRowAction:(PFUser *)user {
+    UITableViewRowAction *rowAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"Unfollow" handler:^(UITableViewRowAction * _Nonnull action, NSIndexPath * _Nonnull indexPath) {
+        [self confirmStopFollowingUser:user];
+    }];
+    
+    rowAction.backgroundColor = kRedColor;
+    return rowAction;
+}
+
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return true;
 }
 
 #pragma mark - SearchTableVC
@@ -140,7 +162,6 @@
 -(void)updateFilteredListsWithString:(NSString*)searchString {
     self.filteredListOfFriendsIveAdded = [self filterList:self.listOfFriendsIveAdded withSearchString:searchString];
     self.filteredListOfFriendsWhoAddedMe = [self filterList:self.listOfFriendsWhoAddedMe withSearchString:searchString];
-    self.filteredPeopleImFollowingNotFollowingMe = [self filterList:self.peopleImFollowingNotFollowingMe withSearchString:searchString];
 }
 
 -(NSMutableArray*)filterList:(NSArray*)list withSearchString:(NSString*)searchString {
@@ -175,7 +196,6 @@
                 [self.listOfFriendsWhoAddedMe addObject:object];
                 [self.friendsWhoAddedMe setObject:object forKey:[object objectId]];
             }
-            [self updateMeFollowingNotMe];
             [self.tableView reloadData];
         }
     }];
@@ -195,19 +215,9 @@
                 [self.listOfFriendsIveAdded addObject:object];
                 [self.friendsIveAdded setObject:object forKey:[object objectId]];
             }
-            [self updateMeFollowingNotMe];
             [self.tableView reloadData];
         }
     }];
-}
-
--(void)updateMeFollowingNotMe {
-    self.peopleImFollowingNotFollowingMe = [[NSMutableArray alloc] init];
-    for (PFUser *friendsFollowing in self.listOfFriendsIveAdded) {
-        if (![self.friendsWhoAddedMe objectForKey:friendsFollowing.objectId]) {
-            [self.peopleImFollowingNotFollowingMe addObject:friendsFollowing];
-        }
-    }
 }
 
 #pragma mark Other
@@ -236,6 +246,35 @@
     [self presentViewController:startFollowing animated:YES completion:nil];
 }
 
+-(void)confirmFollowUser:(PFUser*)user {
+    UIAlertController *startFollowing = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"Follow %@", user.username] message:[NSString stringWithFormat:@"Are you sure you want to start following %@?", user.username] preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *followAction = [UIAlertAction actionWithTitle:@"Follow" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        [self followUserWithUsername:user.username];
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) { }];
+
+    [startFollowing addAction:followAction];
+    [startFollowing addAction:cancelAction];
+    [self presentViewController:startFollowing animated:YES completion:nil];
+}
+
+-(void)confirmStopFollowingUser:(PFUser*)user {
+    UIAlertController *startFollowing = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"Unfollow %@", user.username] message:[NSString stringWithFormat:@"Are you sure you want to stop following %@?", user.username] preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *followAction = [UIAlertAction actionWithTitle:@"Unfollow" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        [self stopFollowingUser:user];
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) { }];
+    
+    [startFollowing addAction:followAction];
+    [startFollowing addAction:cancelAction];
+    [self presentViewController:startFollowing animated:YES completion:nil];
+}
+                                    
+
 -(void)followUserWithUsername:(NSString*)username {
     PFQuery *userForUsername = [PFUser query];
     [userForUsername whereKey:@"username" equalTo:username];
@@ -258,7 +297,7 @@
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
     cell.accessoryType = UITableViewCellAccessoryCheckmark;
     
-    PFUser *user = [super searchIsActive] ? [self.filteredListOfFriendsWhoAddedMe objectAtIndex:indexPath.row] : [self.listOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
+    PFUser *user = [self.getListOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
     [self startFollowingUser:user];
 }
 
@@ -266,9 +305,34 @@
     UITableViewCell *cell = [self.tableView cellForRowAtIndexPath:indexPath];
     cell.accessoryType = UITableViewCellAccessoryNone;
     
-    PFUser *user = [super searchIsActive] ? [self.filteredListOfFriendsWhoAddedMe objectAtIndex:indexPath.row] : [self.listOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
+    PFUser *user = [self.getListOfFriendsWhoAddedMe objectAtIndex:indexPath.row];
+    [self stopFollowingUser:user];
+}
+
+-(void)stopFollowingUser:(PFUser *)user {
+    PFQuery *getFollowEvents = [PFQuery queryWithClassName:@"Following"];
+    
+    [getFollowEvents whereKey:@"user" equalTo:[[PFUser currentUser] objectId]];
+    [getFollowEvents whereKey:@"following" equalTo:user.objectId];
+    [getFollowEvents findObjectsInBackgroundWithBlock:^(NSArray* objects, NSError *error) {
+        if(!error) {
+            [PFObject deleteAllInBackground:objects block:^(BOOL succeeded, NSError * _Nullable error) {
+                if (!error) {
+                    [self.tableView reloadData];
+                }
+                else {
+                    NSLog(@"Could not delete following events");
+                }
+            }];
+        }
+        else {
+            NSLog(@"Unable to stop following users");
+        }
+    }];
+    
     [self.friendsIveAdded removeObjectForKey:[user objectId]];
-    //TODO:Write the code to remove a friend
+    [self.listOfFriendsIveAdded removeObject:user];
+    [super reloadTableView];
 }
 
 -(void)startFollowingUser:(PFUser*)user {
@@ -279,7 +343,6 @@
     
     [self.friendsIveAdded setObject:user forKey:[user objectId]];
     [self.listOfFriendsIveAdded insertObject:user atIndex:0];
-    [self updateMeFollowingNotMe];
     [super reloadTableView];
 }
 
@@ -289,6 +352,14 @@
 
 -(NSString*)description {
     return [NSString stringWithFormat:@"List of Friends VC"];
+}
+
+-(NSMutableArray*)getListOfFriendsIveAdded {
+    return [super searchIsActive] ? self.filteredListOfFriendsIveAdded : self.listOfFriendsIveAdded;
+}
+
+-(NSMutableArray*)getListOfFriendsWhoAddedMe {
+    return [super searchIsActive] ? self.filteredListOfFriendsWhoAddedMe : self.listOfFriendsWhoAddedMe;
 }
 
 @end
